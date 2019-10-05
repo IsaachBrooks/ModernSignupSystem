57ae18367d2519257c8d82c7b03ba1446f2b9f49
isaachbrooks <isaachbrooks@gmail.com>
2019-10-05 12:07:44 -0400

    updated database for many-to-many relation tables

--- website/app/models.py
+++ website/app/models.py
@@ -2,6 +2,11 @@ from datetime import datetime
 from app import db, loginManager
 from flask_login import UserMixin
 
+
+#TODO: make some new tables for Instructor, class priority list, 
+#      class prerequisites, and sectionInstructor
+
+
 @loginManager.user_loader
 def loadUser(user_id):
     return Student.query.get(int(user_id))
@@ -12,6 +17,27 @@ class BaseTable(db.Model):
     created_on = db.Column(db.DateTime, default=db.func.now())
     updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
+student_cur_enroll = db.Table('student_cur_enroll',
+    db.Column('sID', db.Integer, db.ForeignKey('student.sID'), primary_key=True),
+    db.Column('sectcrn', db.Integer, db.ForeignKey('section.crn'), primary_key=True))
+
+classes_prereq = db.Table('classes_prereq',
+    db.Column('prereqCID', db.Integer, db.ForeignKey('classes.cID'), primary_key=True),
+    db.Column('forClassCID', db.Integer, db.ForeignKey('classes.cID'), primary_key=True))
+
+student_classes = db.Table('student_classes',
+    db.Column('cID', db.Integer, db.ForeignKey('classes.cID'), primary_key=True),
+    db.Column('sID', db.Integer, db.ForeignKey('student.sID'), primary_key=True),
+    db.Column('complete', db.Boolean, nullable=False, default=False),
+    db.Column('passed', db.Boolean, nullable=False, default=False),
+    db.Column('grade', db.String(1), nullable=False, default='F'))
+"""
+degree_classes = db.Table('degree_classes',
+    db.Column('degreeID', db.Integer, db.ForeignKey('degree.degreeID'), primary_key=True),
+    db.Column('cID', db.Integer, db.ForeignKey('classes.cID'), primary_key=True),
+    db.Column('priority', db.Integer, nullable=False, default=0))
+"""
+
 class Student(BaseTable, UserMixin):
     sID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     fname = db.Column(db.String(30), nullable=False)
@@ -20,79 +46,76 @@ class Student(BaseTable, UserMixin):
     username = db.Column(db.String(32), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    dID = db.Column(db.Integer, db.ForeignKey('degree.dID'))
+    degreeID = db.Column(db.Integer, db.ForeignKey('degree.degreeID'))
     gpa = db.Column(db.Float, nullable=False, default=0.0)
+    classesTaken = db.relationship('Classes', secondary=student_classes, backref='studentsTaken', lazy=True)
+    classesEnrolled = db.relationship('Section', secondary=student_cur_enroll, backref='students', lazy=True)
 
     def get_id(self):
         return self.sID
 
     def __repr__(self):
-        return f"Student '{self.fname} {self.lname}', id={self.sID}"
+        return f"Student '{self.fname + ' ' + self.lname}', id={self.sID}"
+
+class Faculty(BaseTable):
+    fID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    fname = db.Column(db.String(50), nullable=False)
+    mname = db.Column(db.String(50))
+    lname = db.Column(db.String(50), nullable=False)
+    dpID = db.Column(db.Integer, db.ForeignKey('department.dpID'))
+    sections = db.relationship('Section', backref='instructor', lazy=True)
+
+    def __repr__(self):
+        return f"Faculty '{self.fname + ' ' + self.lname}', id={self.fID}"
 
 class Classes(BaseTable):
     cID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     dpID = db.Column(db.Integer, db.ForeignKey('department.dpID'))
+    degreeID = db.Column(db.Integer, db.ForeignKey('degree.degreeID'))
     cNumber = db.Column(db.Integer, nullable=False)
     sections = db.relationship('Section', backref='sectFor', lazy=True)
     description = db.Column(db.String(1000), nullable=False, default='')
+    prereqs = db.relationship('Classes', secondary=classes_prereq, 
+                            primaryjoin="classes.c.cID==classes_prereq.c.prereqCID",
+                            secondaryjoin="classes.c.cID==classes_prereq.c.forClassCID",
+                            backref='prereqFor', lazy=True)
+    priority = db.Column(db.Integer, nullable=False, default=0)
 
     def __repr__(self):
         return f"cID={self.cID} dpID={self.dpID} cNumber={self.cNumber}"
 
 class Section(BaseTable):
-    crn = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    instructorfname = db.Column(db.String(50), nullable=False)
-    instructormname = db.Column(db.String(50))
-    instructorlname = db.Column(db.String(50), nullable=False)
+    crn = db.Column(db.Integer, primary_key=True, autoincrement=True)  
     cID = db.Column(db.Integer, db.ForeignKey('classes.cID'), nullable=False)
+    iID = db.Column(db.Integer, db.ForeignKey('faculty.fID'))
     days = db.Column(db.String(5), nullable=False, default='MTWRF')
     tStart = db.Column(db.Time, nullable=False, default=datetime.now().time())
     tEnd = db.Column(db.Time, nullable=False, default=datetime.now().time())
     dateStart = db.Column(db.Date, nullable=False, default=datetime.now().date())
     dateEnd = db.Column(db.Date, nullable=False, default=datetime.now().date())
+    capacity = db.Column(db.Integer, nullable=False, default=30)
+    numCurEnrolled = db.Column(db.Integer, nullable=False, default=0)
 
     def __repr__(self):
         return f"crn={self.crn} sectFor={self.sectFor}"
 
 class Degree(BaseTable):
-    dID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    degreeID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     dpID = db.Column(db.Integer, db.ForeignKey('department.dpID'))
     name = db.Column(db.String(100), nullable=False)
     totalHours = db.Column(db.Integer, nullable=False, default=0)
     description = db.Column(db.String(1000), nullable=False, default='')
     students = db.relationship('Student', backref='enrolledDegree', lazy=True)
+    dClasses = db.relationship('Classes', backref='degree', lazy=True)
 
     def __repr__(self):
-        return f"dID={self.dID} name={self.name}"
+        return f"degreeID={self.degreeID} name={self.name}"
 
 class Department(BaseTable):
     dpID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(60), nullable=False)
     code = db.Column(db.String(4), nullable=False)
-
+    classesMember = db.relationship('Classes', backref='department', lazy=True)
     def __repr__(self):
         return f"dpID={self.dpID} name={self.name} code={self.code}"
 
-class StudentClasses(BaseTable):
-    cID = db.Column(db.Integer, db.ForeignKey('classes.cID'), primary_key=True)
-    sID = db.Column(db.Integer, db.ForeignKey('student.sID'), primary_key=True)
-    complete = db.Column(db.Boolean, nullable=False, default=False)
-    passed = db.Column(db.Boolean, nullable=False, default=False)
-    grade = db.Column(db.String(1), nullable=False, default='F')
-
-    def __repr__(self):
-        return f"sID={self.sID} classID={cID} complete={self.complete} grade={self.grade}"
-
-class StudentCurEnroll(BaseTable):
-    sID = db.Column(db.Integer, db.ForeignKey('student.sID'), primary_key=True)
-    sectcrn = db.Column(db.Integer, db.ForeignKey('section.crn'), primary_key=True)
-
-    def __repr__(self):
-        return f"sID={self.sID} sectcrn={self.sectcrn}"
-
-class DegreeClasses(BaseTable):
-    dID = db.Column(db.Integer, db.ForeignKey('degree.dID'), primary_key=True)
-    cID = db.Column(db.Integer, db.ForeignKey('classes.cID'), primary_key=True)
-
-    def __repr__(self):
-        return f"dID={self.dID} classID={self.cID}"
