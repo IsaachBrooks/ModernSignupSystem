25854c4917ee9cd5eb0ecc118df690eb2b2ca868
Isaac Brooks <39911390+IsaachBrooks@users.noreply.github.com>
2019-11-23 17:50:21 -0500

    Delete dbLoadFaculty.py

--- website/dbLoadFaculty.py
+++
@@ -1,8 +0,0 @@
-from app.Scripts.tableLoaderCSVFaculty import facultyFileValidator, facultyFileLoader
-
-filename = 'testdata/faculty-test.csv'
-
-#filename = input('Faculty file CSV: ')
-
-if (facultyFileValidator(filename)):
-    facultyFileLoader(filename)
