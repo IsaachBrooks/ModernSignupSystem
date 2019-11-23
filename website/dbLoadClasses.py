84183777498c5091b711815a0fc647e75abd985c
Isaac Brooks <39911390+IsaachBrooks@users.noreply.github.com>
2019-11-23 17:49:46 -0500

    Delete dbLoadClasses.py

--- website/dbLoadClasses.py
+++
@@ -1,8 +0,0 @@
-from app.Scripts.tableLoaderCSVClasses import classesFileValidator, classesFileLoader
-
-filename = 'testdata/classes-test.csv'
-
-#filename = input('Class file CSV: ')
-
-if (classesFileValidator(filename)):
-    classesFileLoader(filename)
