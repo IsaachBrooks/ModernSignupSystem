3786a473ec3f1f57d3e4e838a5898754849c1aad
Isaac Brooks <39911390+IsaachBrooks@users.noreply.github.com>
2019-11-23 17:49:54 -0500

    Delete dbLoadDegree.py

--- website/dbLoadDegree.py
+++
@@ -1,8 +0,0 @@
-from app.Scripts.tableLoaderCSVDegree import degreeFileValidator, degreeFileLoader
-
-filename = 'testdata/degree-test.csv'
-
-#filename = input('Degree file CSV: ')
-
-if (degreeFileValidator(filename)):
-    degreeFileLoader(filename)
