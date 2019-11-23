af076dcf5a89c61d4b2d42ab32d1ea7dde713744
Isaac Brooks <39911390+IsaachBrooks@users.noreply.github.com>
2019-11-23 17:50:15 -0500

    Delete dbLoadSection.py

--- website/dbLoadSection.py
+++
@@ -1,8 +0,0 @@
-from app.Scripts.tableLoaderCSVSection import sectionFileValidator, sectionFileLoader
-
-filename = 'testdata/section-test.csv'
-
-#filename = input('Section file CSV: ')
-
-if (sectionFileValidator(filename)):
-    sectionFileLoader(filename)
