b7ff61c845b3df4836de24855a99ce5842faabea
Isaac Brooks <39911390+IsaachBrooks@users.noreply.github.com>
2019-11-23 17:50:03 -0500

    Delete dbLoadDegreeClassList.py

--- website/dbLoadDegreeClassList.py
+++
@@ -1,7 +0,0 @@
-from app.Scripts.tableLoaderCSVDegreeClassList import degreeClassesListFileValidator, degreeClassesListFileLoader
-
-filename = 'testdata/Test_DegreeClassList.csv'
-
-#filename = input('Class file CSV: ')
-if (degreeClassesListFileValidator(filename)):
-    degreeClassesListFileLoader(filename)
